565908a607cd78635781cc98a99b80a662cf7e08
Pedro Dias <jpedrodias@gmail.com>
2025-09-23 20:41:30 +0000

    exercício 3

--- aula01/exer03.c
+++ aula01/exer03.c
@@ -6,17 +6,20 @@ Desenvolva um programa que permita converter os dias inseridos em anos.
 
 
 int main() {
-    int dias, anos;
+    int dias, anos, sobras;
     
     // solicitar ao usuário o número de dias
     printf("Digite o número de dias: ");
     scanf("%i", &dias);
     
-    // calcular o número de anos
+    // calcular o número de anos sem considerar anos bissextos
     anos = dias / 365;
-    
+    sobras = dias % 365;
+
     // exibir o resultado
     printf("%i dias correspondem a %i anos.\n", dias, anos);
+    printf("Restam %i dias que não completam um ano.\n", sobras);
+    
     
     return 0;
 }
