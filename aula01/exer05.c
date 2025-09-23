825d0df405527ce1910354ea3006446d4561cdd9
Pedro Dias <jpedrodias@gmail.com>
2025-09-23 21:33:43 +0000

    exercícios 5

--- aula01/exer05.c
+++ aula01/exer05.c
@@ -11,16 +11,16 @@ int main() {
     scanf("%d", &numero);
 
     printf("Tabuada do %d:\n", numero);
-    i = 1;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 2;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 3;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 4;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 5;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 6;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 7;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 8;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 9;  printf("%i x %i = %d\n", numero, i, numero * i);
-    i = 10; printf("%i x %i = %d\n", numero, i, numero * i);
+    i = 1;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 2;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 3;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 4;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 5;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 6;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 7;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 8;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 9;  printf("%d x %d = %d\n", numero, i, numero * i);
+    i = 10; printf("%d x %d = %d\n", numero, i, numero * i);
 
     return 0;
 }
