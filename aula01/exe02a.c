b31100e905683e51ea54fe5b92adf7d89a191dcf
Pedro Dias <jpedrodias@gmail.com>
2025-09-23 22:09:40 +0000

    Exerc]icios 2

---
+++ aula01/exe02a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+1-	Desenvolva um programa que permita fazer um incremento ao valor apresentado pelo utilizador.
+*/
+int main() {
+    int valor;
+
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+
+    valor++;
+
+    printf("Valor incrementado: %d\n", valor);
+
+    return 0;
+}
---
+++ aula01/exe02b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+2-	Desenvolva um programa que permita fazer um incremento de 3 valores ao valor apresentado pelo utilizador.
+*/
+
+
+int main() {
+    int valor;
+
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+
+    valor += 3;
+
+    printf("Valor incrementado: %d\n", valor);
+
+    return 0;
+}
