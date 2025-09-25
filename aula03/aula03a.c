71c667e9b343d431ebd1dc2d6bb2a72b07ddb8af
Pedro Dias <jpedrodias@gmail.com>
2025-09-25 20:51:15 +0000

    Calcula a média das idade de 5 pessoas

--- aula03/aula03a.c
+++ aula03/aula03a.c
@@ -1,11 +1,8 @@
-/*
-Escreve um programa de mostre os primeiro 20 números naturais.
-*/
-
 #include <stdio.h>
 #include <locale.h>
 
 /*
+Escreve um programa de mostre os primeiro 20 números naturais.
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
@@ -18,12 +15,35 @@ int main(){
 
 /*
 Escrever todos os números entre 10 e 100 de 10 em 10
-*/
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
     for(int i = 10; i <= 100; i += 10){
         printf("%d\n", i);
     }
+    return 0;
+}
+*/
+
+/*
+Calcula a média das idade de 5 pessoas
+*/
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int numero_pessoas;
+    int idade, soma= 0;
+    float media;
+
+    printf("Digite número de alunos: ");
+    scanf("%d", &numero_pessoas);
+
+    for(int i = 1; i <= numero_pessoas; i++){
+        printf("Digite a idade da %dª pessoa: ", i);
+        scanf("%d", &idade);
+        soma += idade;
+    }
+    media = (float)soma / numero_pessoas;
+    printf("A média das idades é: %.2f\n", media);
     return 0;
 }
