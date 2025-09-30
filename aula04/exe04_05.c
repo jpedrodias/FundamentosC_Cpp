a8f68a1bb87d47ce994f3dee83158a4bea50d4d6
Pedro Dias <jpedrodias@gmail.com>
2025-09-30 20:51:46 +0000

    Exercício 5

---
+++ aula04/exe04_05.c
@@ -0,0 +1,29 @@
+/*
+5- Validação de entrada
+Escreva um programa que peça ao utilizador um número inteiro positivo.
+O programa deve continuar a pedir até que o utilizador insira um valor menor que 0.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define _MIN_VALUE 0     // não gasta RAM
+const int MIN_VALUE = 0; // gasta RAM
+
+int main(){
+
+    int numero;
+    bool gameOver = false;
+
+    do {
+        
+        printf("Digite um número inteiro positivo: ");
+        scanf("%d", &numero);
+
+        gameOver = (numero < MIN_VALUE);
+        gameOver = (numero < _MIN_VALUE);
+
+
+    } while (!gameOver);
+    return 0;
+}
