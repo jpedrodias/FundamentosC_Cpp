da9379197d090f9107f380d15571e6c4a956f04d
Pedro Dias <jpedrodias@gmail.com>
2025-09-30 21:40:57 +0000

    renaming

--- aula04/exe06_2.c
+++ aula04/exe06_2.c
@@ -1,5 +1,5 @@
 /*
-2.	Listagem de Números Pares e Ímpares
+2. Listagem de Números Pares e Ímpares
 O utilizador insere um número N.
 O programa deve:
 Usar um for para listar todos os números de 1 até N.
