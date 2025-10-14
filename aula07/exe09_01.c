84d5a29b3e8617c660742c7c2de0d3e3335c116d
Pedro Dias <jpedrodias@gmail.com>
2025-10-14 21:29:36 +0000

    Exercício 9.1

--- aula07/exe09_01.c
+++ aula07/exe09_01.c
@@ -21,27 +21,31 @@ Opção: 2
 #include <stdio.h>
 #include <stdbool.h>
 
-
 bool isPrime(int num) {
     if (num <= 1) {
         return false;
-}
+    }
+
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
             return false;
         }
     }
+
     return true;
 }
 
 
 void listPrimesUpTo(int n) {
+
     for (int i = 2; i <= n; i++) {
         if (isPrime(i)) {
             printf("%d ", i);
         }
     }
+
     printf("\n");
+
 }
 
 
--- aula07/exe09_02.c
+++ aula07/exe09_02.c
@@ -10,3 +10,29 @@ Insira um número: 4
 Números pares: 4
 Números ímpares: 6
 */
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int count_number = 0, number;
+    int count_par = 0, count_impar = 0;
+    
+    while (count_number < 10) {
+        printf("Insira um número: ");
+        scanf("%d", &number);
+
+        if (number % 2 == 0) {
+            count_par++;
+        } else {
+            count_impar++;
+        }
+        count_number++;
+    }
+
+    printf("Números pares: %d\n", count_par);
+    printf("Números ímpares: %d\n", count_impar);
+
+    return 0;
+}
