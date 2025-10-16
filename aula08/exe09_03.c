01fa0d1a8809dacdd287cef94277b196708bf54b
Pedro Dias <jpedrodias@gmail.com>
2025-10-16 20:10:06 +0000

    Exercício 9.3

--- aula08/exe09_03.c
+++ aula08/exe09_03.c
@@ -17,26 +17,19 @@ Use:
 #include <stdbool.h>
 
 
-void do_choice_1(), do_choice_2(), do_choice_3();
-
+void faz_escolha_1(), faz_escolha_2(), faz_escolha_3();
+int faz_menu();
 
 int main() {
     int choice;
     do {
 
-        printf("\n===== MENU =====\n");
-        printf("1 - Somar dois números\n");
-        printf("2 - Calcular média de 3 números\n");
-        printf("3 - Verificar se número é positivo ou negativo\n");
-        printf("0 - Sair\n");
-        printf("================\n");
-        printf("Opção: ");
-        scanf("%d", &choice);       
+        choice = faz_menu();     
         
         switch (choice) {
-            case 1: { do_choice_1(); break; }
-            case 2: { do_choice_2(); break; }
-            case 3: { do_choice_3(); break; }
+            case 1: { faz_escolha_1(); break; }
+            case 2: { faz_escolha_2(); break; }
+            case 3: { faz_escolha_3(); break; }
             case 0: break;
             default: printf("Opção inválida. Tente novamente.\n"); 
         }
@@ -46,8 +39,19 @@ int main() {
     return 0;
 } 
 
-
-void do_choice_1() {
+int faz_menu() {
+    int choice;
+    printf("╔═════════════════════════════════╦══════════════════╗\n");
+    printf("║ 1 - Somar dois números          ║ Escolha a opção: ║\n");
+    printf("║ 2 - Calcular média de 3 números ╚══════════════════╣\n");
+    printf("║ 3 - Verificar se número é positivo ou negativo     ║\n");
+    printf("║ 0 - Sair                                           ║\n");
+    printf("╚════════════════════════════════════════════════════╝\n");
+    printf("Opção: ");
+    scanf("%d", &choice);
+    return choice;
+}
+void faz_escolha_1() {
     float a, b;
     printf("Insira dois números para somar:\n");
     scanf("%f %f", &a, &b);
@@ -55,7 +59,7 @@ void do_choice_1() {
 }
 
 
-void do_choice_2() {
+void faz_escolha_2() {
     float x, y, z, media;
     printf("Insira três números para calcular a média:\n");
     scanf("%f %f %f", &x, &y, &z);
@@ -63,7 +67,7 @@ void do_choice_2() {
     printf("Média: %.2f\n", media);
 }
 
-void do_choice_3() {
+void faz_escolha_3() {
     int num;
     printf("Insira um número para verificar se é positivo ou negativo: ");
     scanf("%d", &num);
