d1170d47cf625a407e5df8f98ccdf81fd00d9871
Pedro Dias <jpedrodias@gmail.com>
2025-10-16 20:20:13 +0000

    Exercício 9.4

--- aula08/exe09_04.c
+++ aula08/exe09_04.c
@@ -19,8 +19,17 @@ Sequência Fibonacci: 0 1 1 2 3 5 8
 #include <stdio.h>
 #include <stdbool.h>
 
-void gera_pares(int n), gera_fibonacci(int n), soma_impares(int n);
+
 int faz_menu();
+void gera_pares(int n);
+void soma_impares(int n);
+
+
+// https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci
+void gera_fibonacci(int n);
+void gera_fibonacci_recursivamente(int n, int a, int b);
+
+
 int main() {
     int choice, n;
     do {
@@ -34,7 +43,8 @@ int main() {
 
         switch (choice) {
             case 1: { gera_pares(n); break; }
-            case 2: { gera_fibonacci(n); break; }
+            //case 2: { gera_fibonacci(n); break; }
+            case 2: { gera_fibonacci_recursivamente(n, 0, 1); break; }
             case 3: { soma_impares(n); break; }
             case 0: break;
             default: printf("Opção inválida. Tente novamente.\n"); 
@@ -47,12 +57,13 @@ int main() {
 
 int faz_menu() {
     int choice;
-    printf("╔═════════════════════════════════╦══════════════════╗\n");
-    printf("║ 1 - Sequência de números pares até N       ║ Escolha a opção: ║\n");
-    printf("║ 2 - Sequência de Fibonacci até N termos     ╚══════════════════╣\n");
+    printf("\n");
+    printf("╔═════════════════════════════════════════╦═══╗\n");
+    printf("║ 1 - Sequência de números pares até N    ║ ? ║\n");
+    printf("║ 2 - Sequência de Fibonacci até N termos ╚═══╣\n");
     printf("║ 3 - Soma dos números ímpares até N          ║\n");
-    printf("║ 0 - Sair                                   ║\n");
-    printf("╚════════════════════════════════════════════╝\n");
+    printf("║ 0 - Sair                                    ║\n");
+    printf("╚═════════════════════════════════════════════╝\n");
     printf("Opção: ");
     scanf("%d", &choice);
     return choice;
@@ -82,6 +93,13 @@ void gera_fibonacci(int n) {
     }
     printf("\n");
 }
+
+void gera_fibonacci_recursivamente(int n, int a, int b) {
+    if (n == 0) return;
+    printf("%d ", a);
+    gera_fibonacci_recursivamente(n - 1, b, a + b);
+}
+
 
 void soma_impares(int n) {
     int soma = 0;
