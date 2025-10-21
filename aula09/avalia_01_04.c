e5067b7471a8764d82c9a896fac965f99c0d7852
Pedro Dias <jpedrodias@gmail.com>
2025-10-21 19:29:19 +0000

    exercício 4

--- aula09/avalia_01_04.c
+++ aula09/avalia_01_04.c
@@ -8,3 +8,37 @@ Número: 4582
 O número tem 4 algarismos
 */
 #include <stdio.h>
+#define MAX_INT 2147483647 // 2^31 - 1 (bits)
+
+
+int ler_inteiro_no_intervalo(int minimo, int maximo, const char* mensagem);
+
+
+int main() {
+    int numero, contador = 0;
+
+    printf("Contador de Algarismos\n");
+    numero = ler_inteiro_no_intervalo(1, MAX_INT, "Introduza um número inteiro positivo: ");
+
+    while (numero != 0) {
+        numero /= 10;
+        contador++;
+    }
+
+    printf("O número tem %d algarismos\n", contador);
+    return 0;
+}   
+
+
+int ler_inteiro_no_intervalo(int minimo, int maximo, const char* mensagem) {
+
+    int valor;
+    do {
+        printf("%s", mensagem);
+        scanf("%d", &valor);
+        if (valor < minimo || valor > maximo) {
+            printf("Valor inválido. O valor deve estar entre %d e %d.\n", minimo, maximo);
+        }
+    } while (valor < minimo || valor > maximo);
+    return valor;
+} // fim ler_inteiro_no_intervalo
