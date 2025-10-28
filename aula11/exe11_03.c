fc6323f01429320e657bfd1cfe1215dcfd0bd421
Pedro Dias <jpedrodias@gmail.com>
2025-10-28 22:01:11 +0000

    Exercícios 10

--- aula11/exe11_03.c
+++ aula11/exe11_03.c
@@ -5,7 +5,7 @@ Cria uma função parOuImpar() que recebe um número e diz se é par.
 #include <stdio.h>
 
 void parOuImpar(int numero) {
-    char *resultado = (numero % 2 == 0) ? "par" : "Ímpar";
+    char *resultado = (numero % 2 == 0) ? "par" : "ímpar";
     printf("O número %d é %s.\n", numero, resultado);
 }
 
