18b90acf28e4989a35432ef0a92f27638b6d19a4
Pedro Dias <jpedrodias@gmail.com>
2025-10-30 21:49:15 +0000

    Apontadores

--- aula12/apontadores1.c
+++ aula12/apontadores1.c
@@ -8,12 +8,29 @@ passagem de parâmetros para funções e alocação dinâmica de memória.
 */
 
 #include <stdio.h>
+void aniversario(int *idade);
 
 int main () {
     int idade = 25;
+    int *pIdade = &idade;
 
-    
+    printf("O valor de idade é %d\n", idade);
+    printf("O endereço de idade é %p\n", &idade);
+
+    printf("O valor de pIdade é %p\n", pIdade);
+    printf("O valor referenciado pelo pIdade é %d\n", *pIdade);
+
+    aniversario(&idade);
+
+    printf("O valor de idade é %d\n", idade);
+    printf("O endereço de idade é %p\n", &idade);
     
     return 0;
 }
 
+
+void aniversario(int *idade){
+    //*idade = *idade + 1;
+    (*idade)++;
+    printf("Dentro da função O endereço de idade é %p\n", idade);
+}
