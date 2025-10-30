0e9f22d74727b9cf608dfaf99048e056891bd872
Pedro Dias <jpedrodias@gmail.com>
2025-10-30 20:30:56 +0000

    apontadores

---
+++ aula12/apontadores1.c
@@ -0,0 +1,16 @@
+/*
+Apontadores em C
+Apontadores são variáveis que armazenam o endereço de memória de outra variável. 
+Eles são amplamente utilizados em C para manipulação eficiente de dados, 
+passagem de parâmetros para funções e alocação dinâmica de memória.
+
+// Adaptado de Bro Code https://www.youtube.com/watch?v=DplxIq0mc_Y
+*/
+
+#include <stdio.h>
+
+int main () {
+
+    return 0;
+}
+
--- aula12/apontadores_ensaio.c
+++ aula12/apontadores_ensaio.c
@@ -3,33 +3,36 @@ Apontadores em C
 Apontadores são variáveis que armazenam o endereço de memória de outra variável. 
 Eles são amplamente utilizados em C para manipulação eficiente de dados, 
 passagem de parâmetros para funções e alocação dinâmica de memória.
+
+// Adaptado de Bro Code https://www.youtube.com/watch?v=DplxIq0mc_Y
 */
 
 #include <stdio.h>
-
 void aniversario(int *idade);
 
-int main() {
+int main () {
     int idade = 25;
     int *pIdade = &idade;
 
+    printf("O valor de idade é %d\n", idade);
+    printf("O endereço de idade é %p\n", &idade);
 
-    printf("Idade.valor  = %d\n", idade);
-    printf("Idade.endreço= %p\n", &idade);
 
-    printf("------------------------\n");
-    printf("O valor pIdade %p\n", pIdade);
-    printf("O valor referenciado por pIdade %d\n", *pIdade);
+    printf("O valor de pIdade é %p\n", pIdade);
+    printf("O valor de idade é %d\n", *pIdade);
 
     // & - operador que devolve o endereço de memória da variável
     // * - operador que devolve o valor guardado no endereço de memória 
 
+    // aniversario
     aniversario(&idade);
-    printf("Tens %d anos de idade.\n", idade);
+    printf("O novo valor de idade é %d\n", idade);
+
+    return 0;
 }
 
-
-void aniversario(int *idade) {
+void aniversario(int *idade){
     // *idade = *idade + 1
     (*idade)++;
+    printf("dentro da função o endereço de idade é %p\n", &idade);
 }
